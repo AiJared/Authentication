d5e9b252fe7c35bbdc3bec3b13dfcb28bbb6a2a5
Kerriea-star <ojared511@gmail.com>
2022-06-06 10:10:26 +0300

    Created customUserManager model

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,46 @@ class TrackingModel(models.Model):
     class Meta:
         abstract = True
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None,
+                    is_active=True, is_admin=False, is_staff=False
+                    ):
+        if email is None:
+            raise ValueError("User must have an email!")
+        if username is None:
+            raise ValueError("User must have a username!")
+        if password is None:
+            raise ValueError("User must have a password!")
+        
+        user_obj = self.model(
+            email = self.normalize_email(email),
+            username = username
+        )
+        
+        user_obj.set_password(password)
+        user_obj.is_active = is_active
+        user_obj.is_admin = is_admin
+        user_obj.is_staff = is_staff
+
+        user_obj.save(using=self._db)
+
+        return user_obj
+
+    def create_staff(self, email, username, password=None):
+        user = self.create_user(
+            email, username, password=password,
+            is_active=True,
+            is_admin=False,
+            is_staff=False)
+
+        return user
+    
+    def create_superuser(self, email,username, password=None):
+        user = self.create_user(
+            email, username, password=password,
+            is_active=True,
+            is_admin=True,
+            is_staff=True
+        )
+
+        return user
