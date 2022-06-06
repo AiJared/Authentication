f57b47871068274bc65618dd1b8863b9d2c9ec92
Kerriea-star <ojared511@gmail.com>
2022-06-06 09:53:46 +0300

    Started creating models, created tracking model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth import validators
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext as _
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class TrackingModel(models.Model):
+    date_created = models.DateTimeField(_("date created"), auto_now_add=True)
+    date_updated = models.DateTimeField(_("date updated"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
