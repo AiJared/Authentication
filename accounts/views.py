9d30bdad9c64eb448097a54582513344cf7d5846
Kerriea-star <ojared511@gmail.com>
2022-06-07 12:48:45 +0300

    Created Registration View Set

--- accounts/views.py
+++ accounts/views.py
@@ -61,3 +61,27 @@ class LoginViewSet(ModelViewSet, TokenObtainPairView):
         return Response(serializer.validated_data,
                         status=status.HTTP_200_OK)
     
+class RegistrationViewSet(ModelViewSet, TokenObtainPairView):
+    """
+    Student Registration API View
+    """
+    serializer_class = StudentRegistrationSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ["post"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user_data = serializer.data
+        send_activation_mail(user_data, request)
+        refresh = RefreshToken.for_user(user)
+        res = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token)
+        }
+        return Response({
+            "user": serializer.data,
+            "refresh":res["access"],
+            "token": res["access"]
+        }, status=status.HTTP_201_CREATED)
