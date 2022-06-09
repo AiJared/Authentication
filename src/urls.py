89434ffa48bccd4307f96f16b709e52de9b853e9
Kerriea-star <ojared511@gmail.com>
2022-06-09 12:33:08 +0300

    Created project level urls and started to debug code

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = "Portal"
+API_DESCRIPTION = "Student Portal"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include_docs_urls(title=API_TITLE,
+                                description=API_DESCRIPTION
+                                )),
+    path("api/v1/", include("api.urls")),
 ]
