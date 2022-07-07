2a7899ba862b3d8b50fdb895a4d4560bf10e41cb
Kerriea-star <ojared511@gmail.com>
2022-07-07 16:59:48 +0300

    Debugged some bugs, configured static files, media files and template files

--- src/urls.py
+++ src/urls.py
@@ -16,3 +16,5 @@ urlpatterns = [
                                 )),
     path("api/v1/", include("api.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
